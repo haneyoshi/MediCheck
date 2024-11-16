6d9d6e89ad868ff644773d477ce275b3205a5b72
Tabobaba <pokemonbotno001@gmail.com>
2024-11-15 23:04:13 -0600

    correct ID key and update DB insertion operation

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,60 +1,57 @@
 Create Table Patient(
 patient_id VARCHAR(10),
-firtst_name VARCHAR(20),
-last_name VARCHAR(20),
-date_of_birth date,
+firtst_name VARCHAR(20) NOT NULL,
+last_name VARCHAR(20) NOT NULL,
+date_of_birth DATE NOT NULL,
 primary key(patient_id)
 );
 
 create Table Visit(
   visit_id INT AUTO_INCREMENT PRIMARY KEY,
   patient_id VARCHAR(10),
-  prescription_id INT,
+  visit_date DATETIME,
   FOREIGN KEY (patient_id) REFERENCES Patient(patient_id),
-  FOREIGN KEY (prescription_id) REFERENCES Prescription(prescription_id)
-)
+);
 
 Create Table Symptom(
   sympton_id INT AUTO_INCREMENT PRIMARY KEY,
-  sympton VARCHAR(50)
-)
+  sympton_name VARCHAR(50) NOT NULL,
+  UNIQUE (symptom_name)
+);
 
 -- juncton table
 Create Table VisitSymptom(
+  visit_symptom_id INT AUTO_INCREMENT PRIMARY KEY,
   visit_id INT,
-  sympton_id INT
+  sympton_id INT,
   FOREIGN KEY (visit_id) REFERENCES Visit(visit_id),
   FOREIGN KEY (sympton_id) REFERENCES Symptom(sympton_id)
-)
+);
 
 create table Prescription(
   prescription_id INT AUTO_INCREMENT PRIMARY KEY,
-  visit_id INT
+  visit_id INT,
+  disease_id INT,
   FOREIGN KEY (visit_id) REFERENCES Visit(visit_id),
+  FOREIGN KEY (disease_id) REFERENCES Disease(disease_id)
 );
 
 Create Table Medicine(
   medicine_id INT AUTO_INCREMENT PRIMARY KEY,
-  medicine VARCHAR(50),
-)
+  medicine_name VARCHAR(50) NOT NULL,
+  UNIQUE (medicine_name)
+);
 
 Create Table Disease(
   disease_id INT AUTO_INCREMENT PRIMARY KEY,
-  diease VARCHAR(50)
-)
+  diease_name VARCHAR(50) NOT NULL,
+  UNIQUE (diease_name)
+);
 
 --Junction table
-Create Table PresMedicine(
+Create Table PrescriptionMedicines (
   prescription_id INT,
   medicine_id INT,
   FOREIGN KEY (prescription_id) REFERENCES Prescription(prescription_id),
   FOREIGN KEY (medicine_id) REFERENCES Medicine (medicine_id)
-)
-
---Junction table
-Create Table PresDisease(
-  prescription_id INT,
-  disease_id INT,
-  FOREIGN KEY (prescription_id) REFERENCES Prescription (prescription_id),
-  FOREIGN KEY (disease_id) REFERENCES Disease (disease_id)
-)
+);
