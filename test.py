7b4033682d3bf6b6dd2e2f8108b36cdb72fd9694
Tabobaba <pokemonbotno001@gmail.com>
2024-11-20 19:29:55 -0600

    Add data deletion operation modules

--- test.py
+++ test.py
@@ -1,33 +1,8 @@
-class N:
-    def __init__(self,ranNum):
-        self.n = ranNum
-    def __eq__(self, other):
-        if not isinstance(other,N):
-            return NotImplemented
-        return self.n == other.n
-    
-    def __repr__(self):
-        return f"N({self.n})"
-      
-    
-class T:
-    def __init__(self,ranSrin):
-        self.s = ranSrin
-        
-    
-n1 = N(1)
-n2 = N(2)
-n3 = N(3)
-n4 = N(4)
-n5 = N(5)
-n_3 = N(3)
+from DeletionFormula import delete_patient
+from InsertionFormula import insert_patient
 
-nlist = [n1,n2,n3,n4,n5]
-if n_3 in nlist:
-    print("found")
-    nlist.remove(n3)
-    for n in nlist:
-        print(repr(n))
 
-else:
-    print("not found")
+delete_patient("z567890123")
+
+patient = ("z567890123","Grime","Loryex","1963-04-11")
+insert_patient(patient)
